3f581a3a6ecb5f04402c0c31e75ac3f081bac120
Alexander Vieth <aovieth@gmail.com>
2021-08-20 16:37:27 -0400

    maintainers: add sjamaan

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -20109,6 +20109,11 @@
     githubId = 80012;
     name = "Simon Jagoe";
   };
+  sjamaan = {
+    github = "sjamaan";
+    name = "Peter Bex";
+    githubId = 128536;
+  };
   sjau = {
     email = "nixos@sjau.ch";
     github = "sjau";
