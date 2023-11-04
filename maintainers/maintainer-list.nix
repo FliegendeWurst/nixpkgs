a5697e065d6cfe6222614d12fb3008e340b13d2d
EBADBEEF <errno@ebadf.com>
2023-11-04 12:20:40 -0700

    maintainers: add EBADBEEF

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4393,6 +4393,12 @@
     githubId = 424946;
     name = "James Earl Douglas";
   };
+  EBADBEEF = {
+    name = "EBADBEEF";
+    email = "errno@ebadf.com";
+    github = "EBADBEEF";
+    githubId = 4167946;
+  };
   ebbertd = {
     email = "daniel@ebbert.nrw";
     github = "ebbertd";
