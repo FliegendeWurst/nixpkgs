b58dfa642428f50f489f47ad3d03188dc4fb851f
xyven1 <git@xyven.dev>
2024-05-11 21:17:26 -0400

    python3Packages.pymonocypher: init at 4.0.2.2

---
+++ pkgs/development/python-modules/pymonocypher/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools-scm,
+  cython,
+  numpy,
+}:
+buildPythonPackage rec {
+  pname = "pymonocypher";
+  version = "4.0.2.2";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-wCP+SUnnbPqnl+MMNoLZudGSPbGNL9zr2nU/Wpe3yo4=";
+  };
+
+  build-system = [
+    cython
+    setuptools-scm
+  ];
+
+  dependencies = [ numpy ];
+
+  meta = {
+    description = "Python bindings for Monocypher crypto library";
+    homepage = "https://github.com/jetperch/pymonocypher";
+    license = with lib.licenses; [
+      cc0
+      bsd2
+    ];
+  };
+}
