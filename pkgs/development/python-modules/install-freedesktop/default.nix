abb2d6ec18ff13f3fd453c321486149d1e7b810b
xyven1 <git@xyven.dev>
2024-05-11 21:16:54 -0400

    python3Packages.install-freedesktop: init at 0.1.2

---
+++ pkgs/development/python-modules/install-freedesktop/default.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools-scm,
+}:
+buildPythonPackage rec {
+  pname = "install_freedesktop";
+  version = "0.1.2";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-PlqbNAQXW2nDE7ayjGuiHsgKK0q9lkr4ak3kPDChlfA=";
+  };
+
+  build-system = [
+    setuptools-scm
+  ];
+
+  doCheck = false;
+
+  meta = {
+    description = "Setuptools extension to install freedesktop.org app icons";
+    homepage = "https://github.com/welshjf/install_freedesktop";
+    license = with lib.licenses; [ asl20 ];
+  };
+}
