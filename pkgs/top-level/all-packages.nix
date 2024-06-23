e5f14ba8f2927c639b0d4fad1094c416a098f784
Francis Gagné <fragag1@gmail.com>
2024-06-23 00:19:09 -0400

    wxGTK32: Enable wxWebRequest
    SLADE 3.2.6 uses wxWebRequest. Golly also uses it, and used to enable it
    through an override. This commit enables it by default to reduce the
    number of variants of wxGTK32 that need to be built.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36519,14 +36519,7 @@ with pkgs;
   gravit = callPackage ../applications/science/astronomy/gravit { };
 
   golly = callPackage ../applications/science/misc/golly {
-    wxGTK = wxGTK32.overrideAttrs (x: {
-      configureFlags = x.configureFlags ++ [
-        "--enable-webrequest"
-      ];
-      buildInputs = x.buildInputs ++ [
-        curl
-      ];
-    });
+    wxGTK = wxGTK32;
   };
 
   megam = callPackage ../applications/science/misc/megam {
