928c8dd473391a196837d1eca37c141bdc6a5597
Charles Hovine <charles@hovine.io>
2024-07-31 16:10:12 +0200

    Include suggested changes from review
    - Add setuptools as an explicit build dependency
    - Add `pyproject = true`

--- pkgs/development/python-modules/certbot-dns-linode/default.nix
+++ pkgs/development/python-modules/certbot-dns-linode/default.nix
@@ -1,6 +1,7 @@
 {
   buildPythonPackage,
   pythonPackages,
+  setuptools,
   acme,
   certbot,
   pytestCheckHook,
@@ -9,12 +10,14 @@
 
 buildPythonPackage rec {
   pname = "certbot-dns-linode";
-  format = "setuptools";
 
   inherit (certbot) src version;
   disabled = pythonOlder "3.6";
 
   sourceRoot = "${src.name}/certbot-dns-linode";
+
+  pyproject = true;
+  build-system = [ setuptools ];
 
   propagatedBuildInputs = [
     acme
@@ -27,7 +30,6 @@ buildPythonPackage rec {
   pytestFlagsArray = [ "-o cache_dir=$(mktemp -d)" ];
 
   meta = certbot.meta // {
-    description = "Linode DNS Authenticator plugin for Certbot
-";
+    description = "Linode DNS Authenticator plugin for Certbot";
   };
 }
