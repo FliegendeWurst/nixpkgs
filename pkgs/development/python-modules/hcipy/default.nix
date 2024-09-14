935253e997974367cd946aab5ae5a7ed07171cca
BarrOff <58253563+BarrOff@users.noreply.github.com>
2024-09-14 21:20:47 +0200

    python3Packages.hcipy: init at 0.6.0

---
+++ pkgs/development/python-modules/hcipy/default.nix
@@ -0,0 +1,73 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+
+  # build-system
+  setuptools,
+  setuptools-scm,
+
+  # dependencies
+  numpy,
+  scipy,
+  matplotlib,
+  pillow,
+  pyyaml,
+  astropy,
+  imageio,
+  xxhash,
+  numexpr,
+  asdf,
+
+  # tests
+  pytest-cov-stub,
+  pytestCheckHook,
+  mpmath,
+  dill,
+}:
+
+buildPythonPackage rec {
+  pname = "hcipy";
+  version = "0.6.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-Kt2T6EgCrPTJGiygjmFUh1HH1DthFcZ2mMlnlu0HxsE=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    numpy
+    scipy
+    matplotlib
+    pillow
+    pyyaml
+    astropy
+    imageio
+    xxhash
+    numexpr
+    asdf
+  ];
+
+  pythonImportsCheck = [ "hcipy" ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    pytest-cov-stub
+    mpmath
+    dill
+  ];
+
+  meta = {
+    description = "Framework for performing optical propagation simulations, meant for high contrast imaging";
+    changelog = "https://github.com/ehpor/hcipy/blob/master/doc/changelog.rst";
+    homepage = "https://hcipy.org/";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ BarrOff ];
+  };
+}
