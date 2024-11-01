41b1f2152be7256bf0ecdef2755d7c58192f8fb1
Manu [tennox] <tennox+git@txlab.io>
2024-11-01 17:46:53 +0000

    task-keeper: use upstream instead of fork
    Was required bc. https://github.com/linux-china/task-keeper/pull/5
    which is now merged

--- pkgs/by-name/ta/task-keeper/package.nix
+++ pkgs/by-name/ta/task-keeper/package.nix
@@ -9,16 +9,13 @@ rustPlatform.buildRustPackage {
   version = "0.25.0";
 
   src = fetchFromGitHub {
-    # Using my fork because of https://github.com/linux-china/task-keeper/pull/5
-    owner = "tennox";
+    owner = "linux-china";
     repo = "task-keeper";
-    rev = "4e39d65d01c44b44ae9876d8a3cb37066ce9f3f7";
+    rev = "8b6d5432289dcb90f21b5d2a5d0a0338aa4c95b1";
     hash = "sha256-l/sqYuFrSDJN4Y9NN2fpkEI7AWctezLRi/pNAkPmHRk=";
   };
 
-  cargoLock = {
-    lockFile = "${./.}/Cargo.lock";
-  };
+  cargoHash = "sha256-Dzua9gp8xMcpL8JMvQbLCjE171rxQSKK/mmD58C3DMc=";
 
   # tests depend on many packages (java, node, python, sbt, ...) - which I'm not currently willing to set up 😅
   doCheck = false;
