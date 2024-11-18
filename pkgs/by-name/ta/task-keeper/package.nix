75f426b89d340913bb09ce7a2999149b701c9c67
Manu [tennox] <tennox+git@txlab.io>
2024-11-18 22:47:10 +0000

    task-keeper: 0.25.0 -> 0.27.0
    & apply suggestions of @FliegendeWurst:
    https://github.com/NixOS/nixpkgs/pull/352935#pullrequestreview-2443323084
    
    (and nixfmt)

--- pkgs/by-name/ta/task-keeper/package.nix
+++ pkgs/by-name/ta/task-keeper/package.nix
@@ -2,20 +2,24 @@
   lib,
   rustPlatform,
   fetchFromGitHub,
+  openssl,
+  pkg-config,
 }:
 
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "task-keeper";
-  version = "0.25.0";
+  version = "0.27.0";
 
   src = fetchFromGitHub {
     owner = "linux-china";
     repo = "task-keeper";
-    rev = "8b6d5432289dcb90f21b5d2a5d0a0338aa4c95b1";
-    hash = "sha256-l/sqYuFrSDJN4Y9NN2fpkEI7AWctezLRi/pNAkPmHRk=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-lcwWl1ycMSmHgYT4m+CofwefqnjxjvuJkcv1Pe0OtEo=";
   };
 
-  cargoHash = "sha256-Dzua9gp8xMcpL8JMvQbLCjE171rxQSKK/mmD58C3DMc=";
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ openssl ];
+  cargoHash = "sha256-OVDwaMpA2gw0asdQ+yNRKmcmXLs+ddJI/lodewXujro=";
 
   # tests depend on many packages (java, node, python, sbt, ...) - which I'm not currently willing to set up 😅
   doCheck = false;
