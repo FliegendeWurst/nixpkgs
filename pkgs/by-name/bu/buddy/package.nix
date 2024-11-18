1d2c8dd27a0374c602ff39e02c0510f6bb7a57f7
FliegendeWurst <2012gdwu+github@posteo.de>
2024-11-18 20:56:54 +0100

    buddy: fix with strictDeps = true

--- pkgs/by-name/bu/buddy/package.nix
+++ pkgs/by-name/bu/buddy/package.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
     sha256 = "0dl86l9xkl33wnkz684xa9axhcxx2zzi4q5lii0axnb9lsk81pyk";
   };
 
-  buildInputs = [ bison ];
+  nativeBuildInputs = [ bison ];
   patches = [ ./gcc-4.3.3-fixes.patch ];
   configureFlags = [ "CFLAGS=-O3" "CXXFLAGS=-O3" ];
   env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.cc.isClang "-Wno-error=register";
