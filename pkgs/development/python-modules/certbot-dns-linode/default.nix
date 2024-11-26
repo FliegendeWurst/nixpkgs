d8bb5acb484b58a67d921e87f77d6ce23ec217d6
Charles Hovine <hovinecharles@gmail.com>
2024-11-26 11:12:29 +0100

    python312Packages.certbot-dns-linode: init at 2.11.0

---
+++ pkgs/development/python-modules/certbot-dns-linode/default.nix
@@ -0,0 +1,41 @@
+{
+  buildPythonPackage,
+  pythonPackages,
+  setuptools,
+  acme,
+  certbot,
+  pytestCheckHook,
+  pythonOlder,
+}:
+
+buildPythonPackage rec {
+  pname = "certbot-dns-linode";
+
+  inherit (certbot) src version;
+  disabled = pythonOlder "3.6";
+
+  sourceRoot = "${src.name}/certbot-dns-linode";
+
+  pyproject = true;
+  build-system = [ setuptools ];
+
+  propagatedBuildInputs = [
+    acme
+    certbot
+    pythonPackages.dns-lexicon
+  ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pytestFlagsArray = [
+    "-o cache_dir=$(mktemp -d)"
+
+    # Monitor https://github.com/certbot/certbot/issues/9606 for a solution
+    "-W"
+    "ignore::DeprecationWarning"
+  ];
+
+  meta = certbot.meta // {
+    description = "Linode DNS Authenticator plugin for Certbot";
+  };
+}
