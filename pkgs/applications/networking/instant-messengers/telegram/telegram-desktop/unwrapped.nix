edf179a60a33cf30f857b0827a28c39e0bbcd8eb
FliegendeWurst <2012gdwu+github@posteo.de>
2024-11-28 16:40:18 +0100

    PR'd fixes

--- pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/unwrapped.nix
+++ pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/unwrapped.nix
@@ -35,6 +35,7 @@
   libicns,
   apple-sdk_15,
   nix-update-script,
+  fetchpatch,
 }:
 
 # Main reference:
@@ -55,6 +56,15 @@ stdenv.mkDerivation (finalAttrs: {
     fetchSubmodules = true;
     hash = "sha256-zgvyxhQDF1JcGe/fpputvPrAFh1Z+EGaynSXHDk9k/8=";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/desktop-app/lib_webview/commit/93af01974107d3037b5194b635473201164c112e.patch";
+      hash = "sha256-R/hSbFu4YHcX0+MM9Xxt7RGgxucJchCidelOWPtg8us=";
+      extraPrefix = "Telegram/lib_webview/";
+      stripLen = 1;
+    })
+  ];
 
   postPatch = lib.optionalString stdenv.hostPlatform.isLinux ''
     substituteInPlace Telegram/ThirdParty/libtgvoip/os/linux/AudioInputALSA.cpp \
--- pkgs/development/web/nodejs/v22.nix
+++ pkgs/development/web/nodejs/v22.nix
@@ -24,6 +24,16 @@ buildNodejs {
       url = "https://github.com/nodejs/node/commit/32f7d5ad1cf79e7e731e1bb7ac967f4f2a3194cf.patch?full_index=1";
       hash = "sha256-dyUr3caGfetrXgfAl+CLE1LKKetDZCpPwMg4EM98rqI=";
     })
+    # Fix for https://github.com/NixOS/nixpkgs/issues/355919
+    # FIXME: remove after a minor point release
+    (fetchpatch2 {
+      url = "https://github.com/nodejs/node/commit/a094a8166cd772f89e92b5deef168e5e599fa815.patch?full_index=1";
+      hash = "sha256-5FZfozYWRa1ZI/f+e+xpdn974Jg2DbiHbua13XUQP5E=";
+    })
+    (fetchpatch2 {
+      url = "https://github.com/nodejs/node/commit/f270462c09ddfd770291a7c8a2cd204b2c63d730.patch?full_index=1";
+      hash = "sha256-Err0i5g7WtXcnhykKgrS3ocX7/3oV9UrT0SNeRtMZNU=";
+    })
     # fixes test failure, remove when included in release
     (fetchpatch2 {
       url = "https://github.com/nodejs/node/commit/b6fe731c55eb4cb9d14042a23e5002ed39b7c8b7.patch?full_index=1";
