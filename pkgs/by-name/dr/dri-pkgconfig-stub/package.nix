a76becf1e8ce57e46569f47d21632b1505eee663
K900 <me@0upti.me>
2024-12-06 12:39:29 +0300

    dri-pkgconfig-stub: init
    This is a tiny stub that can be provided to things that just
    want to know where to find DRI drivers for whatever reason
    (looking at you, Chromium), so they don't have to pull in all
    of Mesa.

---
+++ pkgs/by-name/dr/dri-pkgconfig-stub/package.nix
@@ -0,0 +1,14 @@
+{ writeTextFile, mesa }:
+writeTextFile {
+  name = "dri-pkgconfig-stub";
+
+  text = ''
+    dridriverdir=${mesa.driverLink}/lib/dri
+
+    Name: dri
+    Version: ${mesa.version}
+    Description: Nixpkgs graphics driver path stub
+  '';
+
+  destination = "/lib/pkgconfig/dri.pc";
+}
