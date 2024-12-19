2fdba05a964eed54d9e6cd2998cbae0742a2dd62
Marko Müller <93703653+Mx2uller@users.noreply.github.com>
2024-12-19 12:02:28 +0100

    Update pkgs/by-name/sv/sview/package.nix
    Co-authored-by: Wolfgang Walther <walther@technowledgy.de>

--- pkgs/by-name/sv/sview/package.nix
+++ pkgs/by-name/sv/sview/package.nix
@@ -23,7 +23,7 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "gkv311";
     repo = "sview";
-    rev = version;
+    tag = version;
     hash = "sha256-mbEacdBQchziXoZ5vJUiEpa/iHeXeaozte2aXs50/Fo=";
   };
 
