16cbb0af21373d3108c534a1fe289e8ba9f15f24
FliegendeWurst <arne.keller@posteo.de>
2025-01-02 11:55:38 +0100

    python312Packages.pyopengl-accelerate: fix build

--- pkgs/development/python-modules/pyopengl-accelerate/default.nix
+++ pkgs/development/python-modules/pyopengl-accelerate/default.nix
@@ -3,7 +3,7 @@
   buildPythonPackage,
   fetchPypi,
   cython,
-  numpy,
+  numpy_1,
   setuptools,
   wheel,
 }:
@@ -19,9 +19,15 @@ buildPythonPackage rec {
     hash = "sha256-KxI2ISc6k59/0uwidUHjmfm11OgV1prgvbG2xwopNoA=";
   };
 
+  patches = [
+    # fix for GCC 14
+    # https://github.com/mcfletch/pyopengl/pull/112
+    ./gcc14-fix.patch
+  ];
+
   nativeBuildInputs = [
     cython
-    numpy
+    numpy_1
     setuptools
     wheel
   ];
